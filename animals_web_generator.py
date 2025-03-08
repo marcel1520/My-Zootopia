ee4f80e4f5d670542b7d5221ccb8ace043f92bb6
Marcel Mann <marcelmann@marcelsmacmannos.local>
2025-03-08 13:01:21 +0100

    added code to .py new animal.html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_data(file_path):
     '''Loads json file'''
     with open(file_path, "r") as handle:
@@ -16,7 +17,7 @@ def get_animal_data(animal_list):
         character = animal["characteristics"]
         if "type" in character:
             character_type = character["type"]
-            animal_info += (f"name: {animal_name}\n"
+            animal_info += (f"\nname: {animal_name}\n"
                             f"Diet: {diet}\n"
                             f"Location: {location}\n"
                             f"Type: {character_type}\n\n")
@@ -27,10 +28,37 @@ def get_animal_data(animal_list):
     return animal_info
 
 
+def read_html_file(html_animal):
+    '''reads html file'''
+    with open(html_animal, "r") as html_animal_file:
+        animal_html = html_animal_file.read()
+        return animal_html
+
+
+def replace_string(old_string, new_string):
+    '''replaces target string from html file with animal info'''
+    string_to_replace = "__REPLACE_ANIMALS_INFO__"
+    text_to_replace = old_string
+    html_with_replace = text_to_replace.replace(string_to_replace, new_string)
+    return html_with_replace
+
+
+def write_animal_html(animal_html_replaced):
+    '''writes html file with animal data in class'''
+    animal_html = "animal.html"
+    with open(animal_html, "w") as new_animal_file:
+        new_animal_file.write(animal_html_replaced)
+        return new_animal_file
+
+
 def main():
+    '''loads animal data, creates animal string, reads html,
+    replaces html target string with animal data'''
     animal_data = load_data("animals_data.json")
     animal_data_get = get_animal_data(animal_data)
-    print(animal_data_get)
+    html_file_read = read_html_file("animals_template.html")
+    string_replace = replace_string(html_file_read, animal_data_get)
+    animal_html_write = write_animal_html(string_replace)
 
 
 if __name__ == "__main__":
